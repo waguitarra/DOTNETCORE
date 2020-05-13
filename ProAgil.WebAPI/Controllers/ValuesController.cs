66e614423be5fdf6718e949da0a14fd998aac8b6
root <wagner@masip.es>
2020-05-13 17:37:48 +0200

    Return Datos con indetety para Sqlite

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 
@@ -12,52 +13,23 @@ namespace ProAgil.WebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new Evento[] { 
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1ª Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e sua novidades",
-                    Local = "Sao paulo",
-                    Lote = "2ª Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-            };
+            return _context.Eventos.ToList();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<Evento> Get(int id)
         {
-            return new Evento[] { 
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1ª Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e sua novidades",
-                    Local = "Sao paulo",
-                    Lote = "2ª Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-            }.FirstOrDefault(x => x.EventoId == id);
+            return _context.Eventos.ToList().FirstOrDefault(x => x.EventoId == id);
         }
 
         // POST api/values
--- ProAgil.WebAPI/Model/Evento.cs
+++ ProAgil.WebAPI/Model/Evento.cs
@@ -9,5 +9,6 @@ namespace ProAgil.WebAPI.Model
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
+
     }
 }
